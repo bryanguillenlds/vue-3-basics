c766f669e0f92f23acd182ea13d7b7d120984c02
Bryan Guillén <bryanguillenlds@gmail.com>
2025-08-21 20:17:27 -0400

    New indecision app, comparing different ways of setting up components.

---
+++ 03-indecision-app/src/components/MyCounterTrad.vue
@@ -0,0 +1,41 @@
+<template>
+	<section>
+		<h3>{{ count }}</h3>
+		<h3>{{ squaredCount }}</h3>
+
+		<div>
+			<button @click="count++">+1</button>
+			<button @click="count--">-1</button>
+		</div>
+	</section>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue';
+
+export default defineComponent({
+	props: {
+		value: {
+			type: Number,
+			required: true
+		}
+	},
+
+	// Have to pass props to the setup function
+	setup(props) {
+		const count = ref(props.value);
+		const squaredCount = computed(() => count.value * count.value);
+
+		return {
+			count,
+			squaredCount
+		};
+	}
+});
+</script>
+
+<style scoped>
+h1 {
+	color: red;
+}
+</style>
