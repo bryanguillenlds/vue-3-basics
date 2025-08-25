87765560786d9e71ad369654c46d54d7342c7171
Bryan Guillén <bryanguillenlds@gmail.com>
2025-08-25 15:45:23 -0400

    Starting indecision chat application.

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-	<h1 class="text-3xl font-bold underline">Parent Component</h1>
+	<IndecisionView />
 </template>
 
 <script setup lang="ts">
 //setup keyword is so that you don't have to use setup in the script tag
 //this already tells it what to do
+
+import IndecisionView from '@/views/IndecisionView.vue';
 </script>
 
 <style scoped>
